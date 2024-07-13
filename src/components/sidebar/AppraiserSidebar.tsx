96ab87528e6b795e7be17a3bd66051f26e0fcf0f
letrongdao <letrongdaocontact8@gmail.com>
2024-07-13 14:24:01 +0700

    minor update

--- src/components/sidebar/AppraiserSidebar.tsx
+++ src/components/sidebar/AppraiserSidebar.tsx
@@ -7,9 +7,8 @@ export default function AppraiserSidebar() {
   useEffect(() => {
     setPath(window.location.pathname);
   }, [path]);
-return (
+  return (
     <div className="min-h-screen min-w-56 flex flex-col float-left border-r-2 border-black items-start py-4 px-4 gap-4">
-      <div className="w-full border-b border-black my-[-10px]" />
       <Link
         href="/user/appraisers"
         className={`flex items-center w-full h-[8vh] text-sm font-bold px-4 rounded-md mb-[-10%] hover:bg-gray-800 hover:text-white
@@ -18,19 +17,26 @@ return (
         All Appraisal
       </Link>
       <Link
-        href="/user/history"
+        href="/user/appraisers"
         className={`flex items-center w-[92%] h-[6vh] text-xs px-4 my-2 rounded-md ml-4 mb-[-10%] hover:bg-cyan-800 hover:text-white
-        ${path === "/user" ? "bg-cyan-800 text-white" : ""}`}
+        ${path === "/user/appraisers" ? "bg-cyan-800 text-white" : ""}`}
       >
-        History
+        All
       </Link>
       <Link
         href="/user/inAppraisal"
         className={`flex items-center w-[92%] h-[6vh] text-xs px-4 my-2 rounded-md ml-4 mb-[-10%] hover:bg-cyan-800 hover:text-white
         ${path === "/user/inAppraisal" ? "bg-cyan-800 text-white" : ""}`}
       >
-        In Appraisal
+        Pending requests
+      </Link>
+      <Link
+        href="/user/history"
+        className={`flex items-center w-[92%] h-[6vh] text-xs px-4 my-2 rounded-md ml-4 mb-[-10%] hover:bg-cyan-800 hover:text-white
+        ${path === "/user/history" ? "bg-cyan-800 text-white" : ""}`}
+      >
+        History
       </Link>
     </div>
-);
+  );
 }
